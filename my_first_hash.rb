e6c9cab6e992f0c529471303dd1780fd780dd03d
Cameron Cousins <cameroncousins18@gmail.com>
2020-07-16 22:36:35 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,9 +1,8 @@
-def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+def my_hash(yee)
+  yee = {"haw" => "pardner", "hee" => "dino"}
 end
 
+my_hash(yee)
 
 def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
